5713ee9dce5f24e56c657f9dbd037495595823ea
Nkwabyte <nkwabyte@gmail.com>
2025-10-17 10:24:31 +0000

    yes proceed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,9 +10,10 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['Montserrat', 'sans-serif'],
+        headline: ['Montserrat', 'sans-serif'],
         code: ['monospace'],
+        sans: ['var(--font-sans)', 'sans-serif'],
       },
       typography: ({ theme }) => ({
         DEFAULT: {
